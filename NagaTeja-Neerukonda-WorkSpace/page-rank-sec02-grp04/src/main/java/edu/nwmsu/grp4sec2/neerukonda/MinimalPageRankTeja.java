4953f8967b0a2c7de439664b44b8792831f1c0f6
Nagateja Neerukonda <tejausa123@gmail.com>
2022-04-15 16:13:57 -0500

    After running the MinimalPageRankTeja.class and received the output

---
+++ NagaTeja-Neerukonda-WorkSpace/page-rank-sec02-grp04/src/main/java/edu/nwmsu/grp4sec2/neerukonda/MinimalPageRankTeja.java
@@ -0,0 +1,55 @@
+package edu.nwmsu.grp4sec2.neerukonda;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.FlatMapElements;
+import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionList;
+import org.apache.beam.sdk.values.PDone;
+import org.apache.beam.sdk.values.TypeDescriptor;
+
+public class MinimalPageRankTeja {
+
+  public static void main(String[] args) {
+
+    PipelineOptions options = PipelineOptionsFactory.create();
+
+    Pipeline p = Pipeline.create(options);
+
+    String datFolder="web04";
+    String file="go.md";
+   
+   PCollection<KV<String,String>> pColKVpair1 = TejaKVPairMapper(p,"go.md",datFolder);
+   PCollection<KV<String,String>> pColKVpair2 = TejaKVPairMapper(p,"python.md",datFolder);
+   PCollection<KV<String,String>> pColKVpair3 = TejaKVPairMapper(p,"java.md",datFolder);
+   PCollection<KV<String,String>> pColKVpair4 = TejaKVPairMapper(p,"README.md",datFolder);
+
+   
+    PCollectionList<KV<String, String>> pColList = PCollectionList.of(pColKVpair1).and(pColKVpair2).and(pColKVpair3).and(pColKVpair4);
+    PCollection<KV<String, String>> list = pColList.apply(Flatten.<KV<String,String>>pCollections());
+    PCollection<String> pColLinksString = list.apply(MapElements.into(TypeDescriptors.strings()).via((mergeOut)->mergeOut.toString()));
+    pColLinksString.apply(TextIO.write().to("Output_Teja"));  
+    p.run().waitUntilFinish();
+  }
+
+  public static PCollection<KV<String,String>> TejaKVPairMapper(Pipeline p, String filename, String folder){
+   
+    String path = folder + "/" + filename;
+     PCollection<String> pcolInput = p.apply(TextIO.read().from(path));
+     PCollection<String> plink = pcolInput.apply(Filter.by((String line) -> line.startsWith("[")));
+     PCollection<String> pcollectionLinks = plink.apply(MapElements.into((TypeDescriptors.strings()))
+     .via((String linkLine) ->linkLine.substring(linkLine.indexOf("(")+1, linkLine.length()-1)));
+     PCollection<KV<String,String>> pColKVPairs =  pcollectionLinks.apply(MapElements.into(TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.strings()))
+     .via((String outLink) -> KV.of(filename,outLink)));
+    return pColKVPairs;
+  }
+
+}
